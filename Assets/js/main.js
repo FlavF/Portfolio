66205d6020fea155c250c538ef5c264437a849cf
Flavia <flaviaf91@gmail.com>
2021-04-27 16:30:31 +0200

    27 avril 2021
    •	Logo du site materndtreize à mettre en 200px pour src_small
    •	Modal : BackMenu, Backpage
    o	Enlever les boutons modifier sur les images,
    o	meilleures répartitions dans la modal
    o	pb avec backpage (choisir photo : erreur)

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,36 +1,47 @@
 /**
-* *** VARIABLES***********
-* *************************
-*/
+ * *** VARIABLES***********
+ * *************************
+ *
+ * @format
+ */
 
-// **** Pages : Back ****
-let deleted = document.querySelectorAll(".delete");
+// **** Page : Modal ****
+let message = document.getElementById("message"); //open the modal
+let modal = document.getElementById("modal"); //modal
+let hidden = document.getElementsByClassName("hidden"); //modal
+let close = document.getElementsByClassName("close")[0]; //to close modal
 
 /******FUNCTIONS***********
-***************************/
+ ***************************/
 
-// **** Pages : Back ****
-const deleteConfirm = (event) => {
-    confirm = window.confirm('Etes-vous sûr de vouloir supprimer cette entrée ?');
-    // if don't accept delete so no delete
-    if (!confirm) {
-        event.preventDefault();
-    }
+function showModal() {
+	console.log("hello modal");
+	try {
+		modal.classList.remove("hidden");
+		console.log(modal);
+	} catch (error) {
+		console.log("il y a une error");
+	}
+	
 }
 
+function hideModal() {
+	modal.classList.add("hidden");
+}
 
+const closeModal = (event) => {
+	modal.classList.add("hidden");
+};
 
 /*********CODE***********
-***************************/
+ ***************************/
 document.addEventListener("DOMContentLoaded", function () {
+	// open modal
+	message.addEventListener("click", showModal);
 
-
-	// **** Pages : Back ****
-	if (deleted) {
-		for (let y = 0; y < deleted.length; y++) {
-			deleted[y].addEventListener("click", deleteConfirm);
-		}
+	//close modal
+	if (hidden) {
+		close.addEventListener("click", hideModal);
+		window.addEventListener("click", closeModal);
 	}
-
-
-})
+});
